82830cd5ed855ec3aab24de5d9087e03e7f2bf10
WeronikaWolska <weronika.wolska@gmail.com>
2017-06-14 12:58:31 +0200

    XOR w języku naturalnym

--- src/com/pwr/zpi/exceptions/InvalidQuestionException.java
+++ src/com/pwr/zpi/exceptions/InvalidQuestionException.java
@@ -1,7 +1,7 @@
 package com.pwr.zpi.exceptions;
 
 /**
- * Created by Weronika on 25.04.2017.
+ * Exception thrown when asked question has wrong structure
  */
 public class InvalidQuestionException extends Exception {
 
@@ -10,8 +10,7 @@ public class InvalidQuestionException extends Exception {
     public static final int NO_SECOND_TRAIT = 3;
     public static final int NO_OPERATOR = 4;
     public static final int NO_QUESTION = 5;
-    public static final int WRONG_STATES = 6;
-    public static final int WRONG_STRUCTURE = 7;
+    public static final int WRONG_STRUCTURE = 6;
 
     private int mistake = 0;
 
@@ -35,6 +34,9 @@ public class InvalidQuestionException extends Exception {
         return mistake;
     }
 
+    /**
+     * @return String with information what kind of mistake was in the question
+     */
     public String getStringWithInfo()
     {
         switch (mistake)
@@ -44,7 +46,6 @@ public class InvalidQuestionException extends Exception {
             case NO_SECOND_TRAIT: return "No such trait as the second one in this object";
             case NO_OPERATOR: return "No operator";
             case NO_QUESTION: return "When you finally decide what you want to ask, then come again";
-            case WRONG_STATES: return "Negations cannot be used with alternative";
             case WRONG_STRUCTURE: return "question cannot be asked that way";
             case 0: return "Something wrong";
             default: return null;
--- src/com/pwr/zpi/language/LogicOperator.java
+++ src/com/pwr/zpi/language/LogicOperator.java
@@ -15,9 +15,23 @@ public enum LogicOperator {
                 {
                         case AND: return " and ";
                         case OR: return  " or ";
-                        case XOR: return " or ";
+                        case XOR: return " xor ";
                         default: return null;
                 }
+        }
+
+        /**
+         * In a natural language sentence operator XOR is built as 'either ... or",
+         * and this method returns for both OR and XOR 'or' in same place in sentence
+         * @return      string modified for natural language
+         */
+        public String getString()
+        {
+                if(this==AND)
+                        return " and ";
+                if(this==OR || this==XOR)
+                        return " or ";
+                return null;
         }
 
 }
