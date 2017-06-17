64feaef6dec530605b02f933874e39e24524d50a
grzkos <grzesiek621@mixbox.pl>
2017-06-17 14:21:40 +0200

    zmiana w package-info

--- src/com/pwr/zpi/language/package-info.java
+++ src/com/pwr/zpi/language/package-info.java
@@ -1,5 +1,7 @@
 /**
- * //todo
+ * Contains classes responsible for representing and processing modal communication language. Modal communication
+ * language is exact data carrier for formulas processed throughout the entire application.
+ *
  * @author Weronika Wolska
  * @author Grzegorz Kostkowski
  * @author Jarema Radom
